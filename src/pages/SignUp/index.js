db7e787de94b040775dc99ded25ce11008f18d88
Christian Sanches <christian@endless-point.org>
2020-03-12 13:38:23 -0300

    Estilos da página Auth

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,7 +1,20 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-// import { Container } from './styles';
+import logo from '~/assets/gobarber-logo.svg';
 
 export default function SignUp() {
-  return <h1> SignUp </h1>;
+  return (
+    <>
+      <img src={logo} alt="Go Barber" />
+      <form>
+        <input placeholder="Nome Completo" />
+        <input type="email" placeholder="Seu e-mail" />
+        <input type="password" placeholder="Sua senha secreta" />
+
+        <button type="submit">Criar conta</button>
+        <Link to="/">Já tenho login</Link>
+      </form>
+    </>
+  );
 }
