9e3930cdcf4b55ea5b8d8de5a3e9e6e9924a47ca
Christian Sanches <christian@endless-point.org>
2020-03-12 13:50:31 -0300

    Unform nos campos

--- src/pages/SignUp/index.js
+++ src/pages/SignUp/index.js
@@ -1,20 +1,28 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import { Form, Input } from '@rocketseat/unform';
 
 import logo from '~/assets/gobarber-logo.svg';
 
 export default function SignUp() {
+  function handleSubmit(dados) {
+    console.tron.log(dados);
+  }
   return (
     <>
       <img src={logo} alt="Go Barber" />
-      <form>
-        <input placeholder="Nome Completo" />
-        <input type="email" placeholder="Seu e-mail" />
-        <input type="password" placeholder="Sua senha secreta" />
+      <Form onSubmit={handleSubmit}>
+        <Input name="name" placeholder="Nome Completo" />
+        <Input name="email" type="email" placeholder="Seu e-mail" />
+        <Input
+          name="password"
+          type="password"
+          placeholder="Sua senha secreta"
+        />
 
         <button type="submit">Criar conta</button>
         <Link to="/">Já tenho login</Link>
-      </form>
+      </Form>
     </>
   );
 }
